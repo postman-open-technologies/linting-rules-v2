0410c3f83117040fdd735f15dc72b6806a29208f
Arnaud Lauret <arno.di.loreto@gmail.com>
2023-01-26 18:32:52 +0100

    Use --rule=a-rule-name to test a single rule and fixed missing type=object in test file JSON schema

--- tests/refining-rules.test.js
+++ tests/refining-rules.test.js
@@ -1,10 +1,23 @@
 import * as SpectralTestRunner from '../src/tests/spectral-test-runner.js';
 
-//const tests = '**/*.rule-test.yaml';
-//const rulesets = '**/*.spectral-v6.yaml';
-// Targeting only the selected rules in rule
-const title = 'refining OpenAPI rules'
-const tests = './specifications/openapi/sources/refining-rules/**/*.rule-test.yaml';
-const rulesets = './specifications/openapi/sources/refining-rules/**/*.spectral-v6.yaml';
+// Optional --rule=a-rule-name parameter to test only one rule instead of all
+const rule = process.env.npm_config_rule; 
+
+let title;
+let tests;
+let rulesets;
+
+// Testing the rule specified with --rule=a-rule-name
+if(rule !== undefined) {
+  title = `refining OpenAPI rules:${rule}`;
+  tests = `./specifications/openapi/sources/refining-rules/${rule}/*/*.rule-test.yaml`;
+  rulesets = `./specifications/openapi/sources/refining-rules/${rule}/*/*.spectral-v6.yaml`;
+}
+// Testing all rules
+else {
+  title = 'refining OpenAPI rules'
+  tests = './specifications/openapi/sources/refining-rules/**/*.rule-test.yaml';
+  rulesets = './specifications/openapi/sources/refining-rules/**/*.spectral-v6.yaml';
+}
 
 SpectralTestRunner.runTests(tests, rulesets, title);
